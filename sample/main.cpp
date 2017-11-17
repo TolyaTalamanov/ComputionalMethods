af7888d94126238d02043c1d440b7f05f544badc
luba <piccione-mail@yandex.ru>
2017-11-17 18:01:08 +0300

    add gaussian and fix main

--- sample/main.cpp
+++ sample/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <iterator>
 #include "../include/ComputionalMethods.h"
 
 int main() {
     auto computionalMethod = IComputionalMethod::create(Implementation::GAUSS);
-    Matrix m;
-    std::vector<double> b;
-    computionalMethod->compute(m, b);
+    Matrix m = {{1, -1, 1, -1}, {1, 1.5, 2, 4}, {2, 3, 6.5, 10}, {2, -4, 1, -6}};
+    std::vector<double> b{4, 8, 20, 4};
+    std::vector<double>x(4);
+
+    x = computionalMethod->compute(m, b);
+    std::copy(x.begin(), x.end(), std::ostream_iterator<double>(std::cout, "\n"));
     return 0;
 }
+
